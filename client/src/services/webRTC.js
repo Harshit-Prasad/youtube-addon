fd98ba53c985d9ef8ab070dc88fd2376bdbbad6a
Harshit Prasad <harshitprasad532@gmail.com>
2024-03-05 21:51:21 +0530

    fix: adding a turn server

--- client/src/services/webRTC.js
+++ client/src/services/webRTC.js
@@ -3,7 +3,7 @@ class WebRTCPeer {
     this.peer = new RTCPeerConnection({
       iceServers: [
         {
-          urls: ["stun:stun.l.google.com:19302"],
+          urls: ["stun:stun.l.google.com:19302", "turns:freeturn.tel:5349"],
         },
       ],
     });
