69dd683a79674b6894ccb97e678049356d94f121
Harshit Prasad <harshitprasad532@gmail.com>
2024-03-11 22:01:23 +0530

    fix: removed google stun servers

--- client/src/services/webRTC.js
+++ client/src/services/webRTC.js
@@ -4,8 +4,8 @@ export class WebRTCPeer {
       iceServers: [
         {
           urls: [
-            "stun:stun.l.google.com:19302",
-            "stun:stun1.l.google.com:19302",
+            // "stun:stun.l.google.com:19302",
+            // "stun:stun1.l.google.com:19302",
             "stun:global.stun.twilio.com:3478",
           ],
         },
