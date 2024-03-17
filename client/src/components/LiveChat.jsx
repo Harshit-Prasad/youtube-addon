01640f3cdbe213ea55c96c2f52676db6e5ccfbd6
Harshit Prasad <harshitprasad532@gmail.com>
2024-03-17 16:24:16 +0530

    fix: configured live chat embed domain

--- client/src/components/LiveChat.jsx
+++ client/src/components/LiveChat.jsx
@@ -1,12 +1,11 @@
 export default function LiveChat({ streamId }) {
   return (
     <div className="aspect-square max-w-[400px] mx-auto mt-2">
-      live chat code:
       <iframe
         width="520"
         height="616"
         src={`https://www.youtube.com/live_chat?v=${streamId}&amp;embed_domain=${
-          import.meta.env.VITE_CLIENT_URL
+          import.meta.env.VITE_DOMAIN_NAME
         }`}
         className="live-show-chat"
       ></iframe>
