b47b1e15f55ce8d07f302336afe8b78f4c9f6d94
Harshit Prasad <harshitprasad532@gmail.com>
2024-03-23 12:24:28 +0530

    fix: set live chat iframe dimensions

--- client/src/components/LiveChat.jsx
+++ client/src/components/LiveChat.jsx
@@ -2,12 +2,11 @@ export default function LiveChat({ streamId }) {
   return (
     <div className="aspect-square max-w-[400px] mx-auto mt-2">
       <iframe
-        width="520"
-        height="616"
+        width="100%"
+        height="100%"
         src={`https://www.youtube.com/live_chat?v=${streamId}&embed_domain=${
           import.meta.env.VITE_DOMAIN_NAME
         }`}
-        className="live-show-chat"
       ></iframe>
     </div>
   );
